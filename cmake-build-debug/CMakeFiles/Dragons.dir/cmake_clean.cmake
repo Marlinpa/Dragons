f5eb8a61949044ab3c8ff92124b218363150f5fb
Marlinpa <mariannaxlinp@gmail.com>
2024-06-17 02:34:31 +0500

    first commit

---
+++ cmake-build-debug/CMakeFiles/Dragons.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Dragons.dir/main.cpp.obj"
+  "Dragons.exe"
+  "Dragons.exe.manifest"
+  "Dragons.pdb"
+  "libDragons.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Dragons.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
